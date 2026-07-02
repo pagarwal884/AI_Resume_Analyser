af7f3808b68db7a8d45c0f6e75699daba826b61a
pagarwal884 <pagarwal1145@gmail.com>
2026-07-02 15:08:42 +0530

    Add few pre build files and few ;logic to map dummy data to my webapp for testing with ai file format data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,8 @@
+import Navbar from "~/components/Navbar";
+import { resumes } from "../constants";
 import type { Route } from "./+types/home";
+import type { Resume } from "../types/index";
+import ResumeCard from "~/components/ResumeCard";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -9,12 +13,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+  <Navbar />
+
     <section className="main-section">
       <div className="page-heading">
         <h1 className="">Track Your Applications & Resume Ratings</h1>
         <h2>Review your submission and check AI-powered feedback.</h2>
       </div>
     </section>
+
+    {resumes.map((resume: Resume) => (
+  <ResumeCard key={resume.id} resume={resume} />
+))}
   </main>
 }
